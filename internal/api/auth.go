5a09d5e7250f3f98b0c71ce1c10622b5abc450e3
Erik Garces Matos <erik_senac@hotmail.com>
2024-12-26 23:03:32 -0300

    feat: Adding products table

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -4,12 +4,18 @@ import (
 	"net/http"
 
 	"github.com/erikgmatos/gobid/internal/jsonutils"
+	"github.com/gorilla/csrf"
 )
 
-func (api *Api) AuthMiddleware(next http.Handler) http.Handler {
+func (api *Api) HandleGetCsrfToken(w http.ResponseWriter, r *http.Request) {
+	token := csrf.Token(r)
+	jsonutils.EncodeJson(w, r, http.StatusOK, map[string]any{"csrf_token": token})
+}
+
+func (api *Api) authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !api.Sessions.Exists(r.Context(), "AuthenticatedUserId") {
-			jsonutils.EncodeJson(w, r, http.StatusUnauthorized, map[string]any{"error": "must be logged in"})
+			jsonutils.EncodeJson(w, r, http.StatusUnauthorized, map[string]string{"error": "must be logged in"})
 			return
 		}
 		next.ServeHTTP(w, r)
